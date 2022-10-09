4494be2bfebb0f328727a54f71d79818bd079ba3
Shrvangowda <114823068+Shrvangowda@users.noreply.github.com>
2022-10-09 15:49:26 +0530

    Add files via upload

---
+++ Comment.js
@@ -0,0 +1,27 @@
+/* comments:non-executable instructions that only use
+inside the source code to express whats going on 
+in the source code
+*/
+
+// In javascript there are two kinds of comments:
+
+// 1.Single line comments
+// 2. Multiline comments
+
+/* Single line comments */
+
+function foo() {
+  console.log("This line executed from function foo");
+}
+
+// the above function display a statement
+
+function bar() {
+  console.log("Hello User");
+}
+
+/* Multiline comment */
+
+/* The above bar
+function greet a user with Hello 
+User Message */
